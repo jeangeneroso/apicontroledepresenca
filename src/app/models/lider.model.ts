a833e8d1bffc4503bd494a429754998215744c53
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-04 13:13:13 -0300

    Ajuste na model ider

--- src/app/models/lider.model.ts
+++ src/app/models/lider.model.ts
@@ -1,2 +1,9 @@
 export interface LiderModel {
+    id?: number | string;
+    nomeLider: string;
+    rgLider: string;
+    cpfLider: string;
+    chavePix: string;
+    valorDiaria: number | string;   // Ex: 160.00
+    valorHoraExtra: number | string; // Ex: 15.00
 }
