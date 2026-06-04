39060367fba3761295c59524627f2690cf0e5bb7
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-04 13:19:24 -0300

    Ajustes no modulo lider no projeto

--- src/app/lideres/lideres-routing.module.ts
+++ src/app/lideres/lideres-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LideresComponent } from './lideres.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+{ path: '', component: LideresComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/lideres/lideres.module.ts
+++ src/app/lideres/lideres.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LideresRoutingModule } from './lideres-routing.module';
 import { LideresComponent } from './lideres.component';
+import { AppMarterialModule } from '../compartilhado/app-marterial/app-marterial.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { LideresComponent } from './lideres.component';
   ],
   imports: [
     CommonModule,
-    LideresRoutingModule
+    LideresRoutingModule,
+    AppMarterialModule
   ]
 })
 export class LideresModule { }
