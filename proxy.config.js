20e92292019449a4650164cac99ef1ac9287c10d
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-23 18:18:35 -0300

    Inclusão do arquivo proxy.config.js

---
+++ proxy.config.js
@@ -0,0 +1,13 @@
+const PROXY_CONFIG = [
+  {
+    context: ['/api'],
+    target: 'http://localhost:8080',
+    secure: false,
+    logLevel: 'debug',
+    changeOrigin: true,
+    // ADICIONE ESSA LINHA ABAIXO:
+    pathRewrite: { '^/api': '' } 
+  }
+];
+
+module.exports = PROXY_CONFIG;
