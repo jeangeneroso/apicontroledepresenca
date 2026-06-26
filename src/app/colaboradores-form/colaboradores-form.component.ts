16d24c85d5def61f9429bfee5a41f8c9f4c2efbc
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-26 13:45:43 -0300

    Ajustes no modulo formulario

--- src/app/colaboradores-form/colaboradores-form.component.ts
+++ src/app/colaboradores-form/colaboradores-form.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { AppMarterialModule } from '../compartilhado/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-colaboradores-form',
   standalone: true,
   imports: [
     CommonModule,
-    AppMarterialModule,
-    ReactiveFormsModule
+    ...AppMarterialModule,
+
   ],
   templateUrl: './colaboradores-form.component.html',
   styleUrl: './colaboradores-form.component.css'
 })
-export class ColaboradoresFormComponent {
+export class ColaboradoresFormComponent implements OnInit {
+
+  form: FormGroup
+formLider: any;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+
+      nomeColaborador: [],
+      rgColaborador: [],
+      cpfColaborador: [],
+      chavePix: []
+    });
+
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
+
+  onCancel() {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
