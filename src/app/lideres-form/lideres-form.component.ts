642a3fbbe07027b48ac885215271ea35fce6ad7b
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-04 17:01:36 -0300

    Ajustes no modulo e componente lideres

--- src/app/lideres-form/lideres-form.component.ts
+++ src/app/lideres-form/lideres-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AppMarterialModule } from '../compartilhado/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { LideresService } from '@services/lideres.service';
@@ -23,7 +24,8 @@ export class LideresFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: LideresService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
 
   ) {
 
@@ -49,16 +51,18 @@ export class LideresFormComponent implements OnInit {
     this.snackBar.open(' Lider cadastrado com sucesso! ', ' Fechar ', {
       duration: 5000
     });
+    
   }
 
   private onError() {
     this.snackBar.open(' Erro ao salvar o lider ', ' Fechar ', {
       duration: 5000
     });
+    this.onCancel();
   }
 
   onCancel() {
-    throw new Error('Method not implemented.');
+    this.location.back();
   }
 
   ngOnInit(): void {
