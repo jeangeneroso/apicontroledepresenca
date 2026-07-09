3c385aed36663418c61da34fa7bcaeddf9becaa4
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-09 16:37:36 -0300

    Inclusao do modulo operacoes

---
+++ src/app/operacoes/operacoes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operacoes',
+  imports: [],
+  templateUrl: './operacoes.component.html',
+  styleUrl: './operacoes.component.css'
+})
+export class OperacoesComponent {
+
+}
---
+++ src/app/operacoes/operacoes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OperacoesRoutingModule } from './operacoes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OperacoesRoutingModule
+  ]
+})
+export class OperacoesModule { }
