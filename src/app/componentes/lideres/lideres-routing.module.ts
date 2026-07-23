69ffca879bfd947cfcdf0dcc8849e15b6d6fc1cc
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-22 23:43:32 -0300

    feat: ajustes no routing.modules

--- src/app/componentes/lideres/lideres-routing.module.ts
+++ src/app/componentes/lideres/lideres-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LideresComponent } from './lideres.component';
 import { LideresFormComponent } from '../../containers/lideres-form/lideres-form.component'
+import { lideresResolver } from '../../guards/lideres.resolver';
 
 const routes: Routes = [
 
   { path: '', component: LideresComponent },
-  { path: 'new',component:LideresFormComponent}
+  { path: 'new',component:LideresFormComponent},
+  { path: 'edit/:id',component:LideresFormComponent, resolve: {Lider: lideresResolver}}
 
 ];
 
