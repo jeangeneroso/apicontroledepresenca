821f807a8a5cc9dc0c18c5d9c5052057f1e88a28
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-23 22:17:44 -0300

    feat: ajustes nas rotas

--- src/app/componentes/colaboradores/colaboradores-routing.module.ts
+++ src/app/componentes/colaboradores/colaboradores-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ColaboradoresComponent } from './colaboradores.component';
 import { ColaboradoresFormComponent } from '../../containers/colaboradores-form/colaboradores-form.component';
-import { colaboradoresResolver } from '../../guards/colaboradores.resolver';
+import { ColaboradoresResolver } from '../../guards/colaboradores.resolver';
 
 const routes: Routes = [
 
     { path: '', component: ColaboradoresComponent },
     { path: 'new', component: ColaboradoresFormComponent },
-    { path: 'edit/:id', component: ColaboradoresFormComponent, resolve: {Colaborador: colaboradoresResolver} }  
+    { path: 'edit/:id', component: ColaboradoresFormComponent, resolve: {colaborador: ColaboradoresResolver} }  
 ];
 
 
--- src/app/componentes/operacoes/operacoes-routing.module.ts
+++ src/app/componentes/operacoes/operacoes-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OperacoesComponent } from './operacoes.component';
 import { OperacoesFormComponent } from '../../containers/operacoes-form/operacoes-form.component';
-import { operacoesResolver } from '../../guards/operacoes.resolver';
+import { OperacoesResolver } from '../../guards/operacoes.resolver';
 
 
 const routes: Routes = [
 
   { path: '', component: OperacoesComponent },
   { path: 'new', component: OperacoesFormComponent },
-  { path: 'edit/:id', component: OperacoesFormComponent, resolve: {Operacao:operacoesResolver}}
+  { path: 'edit/:id', component: OperacoesFormComponent, resolve: {operacao:OperacoesResolver}}
 
 ];
 
