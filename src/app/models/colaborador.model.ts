afbd60924d2b6034dc9946a85f37ddbe0b5d63b8
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-30 14:30:49 -0300

    fix:  Ajustes no model

--- src/app/models/colaborador.model.ts
+++ src/app/models/colaborador.model.ts
@@ -1,6 +1,7 @@
 export interface Colaborador {
   id?: number | string;
   nomeColaborador: string;
+  nome?: string; //
   rgColaborador: string;
   cpfColaborador: string;
   chavePix: string;
--- src/app/models/lider.model.ts
+++ src/app/models/lider.model.ts
@@ -3,6 +3,7 @@ export interface Lider {
     id?: number | string;
     nomeLider: string;
     rgLider: string;
+    nome?: string; //
     cpfLider: string;
     chavePix: string
 }
